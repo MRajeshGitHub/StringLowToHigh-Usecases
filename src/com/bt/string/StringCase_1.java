ad02bf5b8da440f7ca925fc476cc1bc61dddb7fb
ADMIN <ADMIN@DESKTOP-BFTQ2K6>
2024-02-19 10:52:58 +0530

    BT-55252-String charAt method use-cases

--- src/com/bt/string/StringCase_1.java
+++ src/com/bt/string/StringCase_1.java
@@ -4,6 +4,13 @@ public class StringCase_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("String charAt method usecase.");
+		
+		//taking variable as string,In java String is Object and to manipulate we have inbuilt methods..
+		String s="String";
+		//get 3rd index of String
+		System.out.println("Thired char in String is :"+s.charAt(3));
+		System.out.println("Fifth char in String is :"+s.charAt(5));
 
 	}
 
